aa5c914f6cd13fdbc9f8e9ad2dcf267b0d5cd50f
gnanu45229 <gnanendrachinthamani@gmail.com>
2025-06-01 13:38:18 +0530

    Initial commit for sumUp_Web project

---
+++ src/main/java/cys/gnanu/SumUp/SumUpApplication.java
@@ -0,0 +1,13 @@
+package cys.gnanu.SumUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cys/gnanu/SumUp/SumUpApplicationTests.java
@@ -0,0 +1,13 @@
+package cys.gnanu.SumUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
